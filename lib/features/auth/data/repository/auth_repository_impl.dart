8285e29136afa4d2be5dc2a133ab77974ff14c75
PSHEYH <pabloshulz@ukr.net>
2026-01-25 17:42:18 +0200

    Fixing contact us request and user esim list request. Added README for starting project. Fix auth_repository_impl.dart.

--- lib/features/auth/data/repository/auth_repository_impl.dart
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -28,7 +28,7 @@ final class AuthRepositoryImpl  implements AuthRepository {
       final GoogleSignInAccount googleUser = await GoogleSignIn.instance.authenticate(scopeHint: ['email']);
 
       final GoogleSignInClientAuthorization? clientAuthorization = await googleUser.authorizationClient.authorizationForScopes(['email']);
-      // String acceess_token = "ya29.a0ATi6K2sengWjsJaR8jd2KnGymTZZYJG4UpHaGvDNRkN8rW3GID4AgkUzS7T72zjc-Ko95cPt9yVUOEzt3E2vW3mcDolOV7BX1t4QXwVhslMtH5D66dBCbNoBRHveG1vUeVpjsq-SXHrTgG4PbgnlIfzOswCemp2NnmqBbNKOgARIp4XHTLSaO5INCmKhE4M_1eqhQ57wVrcApzIxJdEXzZKrwE473eIWv5aiXa04-56aLt0syu12_6jbRySZHDaBNyPLgaSOUfcdwcUaCgYKAa4SARcSFQHGX2MiVzm7wD9fl0RPQFRvzd-fWw0278";
+
 
       if (clientAuthorization == null) {
         throw Exception('Google authorization cancelled or failed.');
@@ -41,7 +41,6 @@ final class AuthRepositoryImpl  implements AuthRepository {
         },
       );
 
-      // return TokenModel(accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjYzLCJleHAiOjE3NjQ3NjAyMDV9.nYZIf1l-xdu2ZcNx68iiQoBf67M17V8XdsnuKJqCZZY", refreshToken: "SFUIIAGvmEbL/YUpyJio6HYZXOiEw5rbYlH1J7G4J0cpIXRS58ovXORYFsMusVj6", accessTokenExpirationAt: "2025-12-17T10:10:05.060+00:00", refreshTokenExpirationAt: "2025-12-03T11:10:05.060+00:00");
     } catch (e, st) {
       debugPrint('❌ Google Sign-In Error: $e');
       debugPrintStack(stackTrace: st);
